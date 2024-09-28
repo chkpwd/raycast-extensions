5cc15d149f319073b0439cb0f36baf053802b557
Bryan Jones <unix.chkpwd@gmail.com>
2024-09-27 20:07:37 -0400

    removed alfred stuff

--- ente-auth/src/helper/types.ts
+++ ente-auth/src/helper/types.ts
@@ -1,8 +1,3 @@
-enum OutputFormat {
-  ALFRED = 'alfred',
-  JSON = 'json',
-}
-
 interface Secret {
   username: string;
   secret: string;
@@ -12,20 +7,12 @@ interface SecretData {
   [serviceName: string]: Secret[];
 }
 
-interface AlfredFormat {
-  title: string;
-  subtitle: string;
-  arg: string;
-  icon: { path: string };
-}
-
 interface JsonFormat {
   service_name: string;
   current_totp: string;
   current_totp_time_remaining: number;
   next_totp: string;
-  service_data: string;
+  username?: string;
 }
 
-export type { Secret, SecretData as ServiceData, AlfredFormat, JsonFormat };
-export { OutputFormat as OutputTypes };
+export type { Secret, SecretData as ServiceData, JsonFormat };
