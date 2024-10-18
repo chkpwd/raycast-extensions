c68b0ab3b96fddfbb051c2e390c70fae57b76b5e
Bryan Jones <unix.chkpwd@gmail.com>
2024-10-18 10:25:14 -0400

    add new constant

--- ente-auth/src/constants/ente.ts
+++ ente-auth/src/constants/ente.ts
@@ -2,4 +2,6 @@ import path from "path";
 import { getPreferenceValues } from "@raycast/api";
 
 export const DEFAULT_EXPORT_PATH =
-  getPreferenceValues().exportPath || path.join(process.env.HOME || "", "Documents", "ente");
+getPreferenceValues().exportPath || path.join(process.env.HOME || "", "Documents", "ente");
+
+export const EXPORTPATH = getPreferenceValues().exportPath || `${DEFAULT_EXPORT_PATH}/ente_auth.txt`;
